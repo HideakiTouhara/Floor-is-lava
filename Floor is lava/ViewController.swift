5ecc660e2c379b72911910105d3e81560ac98999
HideakiTouhara <hideee6723@gmail.com>
2017-11-07 23:46:00 +0900

    Detect horizontal

--- Floor is lava/ViewController.swift
+++ Floor is lava/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        configuration.planeDetection = .horizontal
         sceneView.session.run(configuration)
     }
 
