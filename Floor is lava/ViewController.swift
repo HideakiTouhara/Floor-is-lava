caf27c8ba91692d422d309cb1b7bd26b2268199b
HideakiTouhara <hideee6723@gmail.com>
2017-11-12 18:25:22 +0900

    Create lava

--- Floor is lava/ViewController.swift
+++ Floor is lava/ViewController.swift
@@ -20,11 +20,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         configuration.planeDetection = .horizontal
         sceneView.session.run(configuration)
         sceneView.delegate = self
+        let lavaNode = createLava()
+        sceneView.scene.rootNode.addChildNode(lavaNode)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func createLava() -> SCNNode {
+        let lavaNode = SCNNode(geometry: SCNPlane(width: 1, height: 1))
+        lavaNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "lava")
+        lavaNode.geometry?.firstMaterial?.isDoubleSided = true
+        lavaNode.position = SCNVector3(0,0,1)
+        lavaNode.eulerAngles = SCNVector3(90.degreesToRadians,0,0)
+        return lavaNode
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
@@ -38,9 +49,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-        <#code#>
+        
     }
+}
 
-
+extension Int {
+    var degreesToRadians: Double { return Double(self) * Double.pi / 180 }
 }
 
